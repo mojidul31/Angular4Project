1eeaae4e8b3bf857698490db0a7575918f817bf5
mojidul31 <mojidul31@gmail.com>
2017-09-11 15:37:19 +0600

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,37 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+/* template:`<h1>{{title}}</h1>
+<span>Hello {{firstName}} {{lastName}}</span>`,
+styles:['h1{color:green}','span{color:navy}'] */
 })
 export class AppComponent {
-  title = 'app';
+  public title = 'Development Bank Limited';
+  public firstName = 'Mojidul';
+  public lastName = 'Islam';
+  //public heroImageUrl='D:\QA ALL\Images\1.jpg';
 }
+/* export const routerConfig: Routes = [
+  {
+      path: 'home',
+      component: HomeComponent
+  },
+  {
+      path: 'about',
+      component: AboutComponent
+  },
+  {
+      path: 'courses',
+      component: CoursesComponent
+  },
+  {
+      path: '',
+      redirectTo: '/home',
+      pathMatch: 'full'
+  },
+  {
+      path: '**',
+      redirectTo: '/home',
+      pathMatch: 'full'
+  }
+]; */
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
+import{AlertModule } from 'ngx-bootstrap';
+import { EmployeeItemComponent } from './employee-item/employee-item.component';
+import { EmployeeMenuComponent } from './employee-menu/employee-menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeItemComponent,
+    EmployeeMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]    
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
